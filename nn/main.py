be6637a205a1b647d56493972ebfbcb9fc60b45c
KATSU-dev <55807384+KATSU-dev@users.noreply.github.com>
2021-05-16 16:54:54 +0100

    Functional Basic Classes

--- nn/main.py
+++ nn/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from nn_lib.data_parsing import DataParser
 from nn_lib.nn_model import NNModel
 
@@ -9,14 +10,13 @@ if __name__ == '__main__':
                                                             split_months=6,
                                                             past_displacement=14, 
                                                             future_displacement=1)    
-    model = NNModel("E.h5")
-    # model.add_training_data(trainX=trainX, trainY=trainY, scaler = data_parser.scaler, is_split=split_data)
-    # model.addLayers(number=1, units=32, activation='tanh', dropout=0.35)
+    model = NNModel()
+    model.add_training_data(trainX=trainX, trainY=trainY, scaler = data_parser.scaler, is_split=split_data)
+    model.addLayers(number=2, units=128, activation='tanh', dropout=0.35)
     model.compile()
-
-    # model.save("E.h5")
-
-    # model.train(epochs=2000, is_split=split_data)
+    model.train(epochs=1200, is_split=split_data)
     
+    model.save(str(np.round(time.time()*1000))+".h5")
+
     # past_data = np.asarray(trainX[len(trainX)-1]).astype(np.float32)
     # print(model.predict(past_data=past_data, future_displacement=7))
